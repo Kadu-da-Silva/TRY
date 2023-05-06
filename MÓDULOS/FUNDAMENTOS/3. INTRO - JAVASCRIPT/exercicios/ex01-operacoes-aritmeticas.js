e68d6b21803c67035c57657062409e2ebf305c9d
Carlos Eduardo <carlao0010@hotmail.com>
2023-05-06 17:28:49 -0300

    Create ex01-operacoes-aritmeticas.js

---
+++ MÓDULOS/FUNDAMENTOS/3. INTRO - JAVASCRIPT/exercicios/ex01-operacoes-aritmeticas.js
@@ -0,0 +1,37 @@
+// 1 - Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+// Adição
+// Subtração
+// Multiplicação
+// Divisão
+// Módulo
+
+const a = 10;
+const b = 20;
+const sum = a + b;
+
+console.log(`${a} + ${b} = ${sum}`);
+
+const c = 25;
+const d = 5;
+const sub = c - d;
+
+console.log(`${c} - ${d} = ${sub}`);
+
+const e = 2;
+const f = 17;
+const mul = e * f;
+
+console.log(`${e} * ${f} = ${mul}`);
+
+
+const g = 256;
+const h = 2;
+const div = g / h;
+
+console.log(`${g} / ${h} = ${div}`);
+
+const i = 11;
+const j = 2;
+const mod = i % j;
+
+console.log(`${i} % ${j} = ${mod}`);
